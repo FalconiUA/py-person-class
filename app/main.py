fa32e8035ba7dee53b9411c3e2f69827375795f5
Andrii Sokolan <your@mail.com>
2024-12-07 13:24:04 +0200

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[self.name] = self
@@ -14,18 +14,22 @@ def create_person_list(people: list) -> list:
         age = person_dict["age"]
         person = Person(name, age)
         person_list.append(person)
+
     for person_dict in people:
         person = Person.people[person_dict["name"]]  # Отримуємо відповідний екземпляр
         if "wife" in person_dict and person_dict["wife"]:
-            wife = Person.people.get(person_dict["wife"])
+            wife = Person.people.get(
+                person_dict["wife"]
+            )
             person.wife = wife
             if wife:
                 wife.husband = person
         if "husband" in person_dict and person_dict["husband"]:
-            husband = Person.people.get(person_dict["husband"])
+            husband = Person.people.get(
+                person_dict["husband"]
+            )
             person.husband = husband
             if husband:
                 husband.wife = person
 
     return person_list
-#test

837335a481bfb1d230d687d4c862f46ba7a25de5
Andrii Sokolan <your@mail.com>
2024-12-07 13:39:07 +0200

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -7,27 +7,26 @@ class Person:
         Person.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
+def create_person_list(people: list[dict[str, str | int]]) -> list[Person]:
     person_list = []
+
+    # Перший цикл: створення екземплярів
     for person_dict in people:
         name = person_dict["name"]
         age = person_dict["age"]
         person = Person(name, age)
         person_list.append(person)
 
+    # Другий цикл: встановлення wife і husband
     for person_dict in people:
-        person = Person.people[person_dict["name"]]  # Отримуємо відповідний екземпляр
-        if "wife" in person_dict and person_dict["wife"]:
-            wife = Person.people.get(
-                person_dict["wife"]
-            )
+        person = Person.people[person_dict["name"]]
+        if wife_name := person_dict.get("wife"):
+            wife = Person.people.get(wife_name)
             person.wife = wife
             if wife:
                 wife.husband = person
-        if "husband" in person_dict and person_dict["husband"]:
-            husband = Person.people.get(
-                person_dict["husband"]
-            )
+        if husband_name := person_dict.get("husband"):
+            husband = Person.people.get(husband_name)
             person.husband = husband
             if husband:
                 husband.wife = person
